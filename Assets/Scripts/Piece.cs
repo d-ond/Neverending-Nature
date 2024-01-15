c0dba9da6b28dafe64194ea0ecdc2a2dd4123c8b
dndorc <116017849+dndorc@users.noreply.github.com>
2024-01-14 21:46:27 -0500

    basics of tetris implemented
    pieces can be moved left or right, hard drop and soft drop implemented
    
    likely skipping rotation because I am more concerned with steps and locks

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -22,4 +22,54 @@ public class Piece : MonoBehaviour
             this.cells[i] = (Vector3Int) data.cells[i];
         }
     }
+
+    private void Update()
+    {
+        this.board.Clear(this);
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Move(Vector2Int.left);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            Move(Vector2Int.right);
+        }
+
+        if (Input.GetKeyDown (KeyCode.S))
+        {
+            Move(Vector2Int.down);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            HardDrop();
+        }
+
+        this.board.Set(this);
+    }
+
+    private void HardDrop()
+    {
+        while(Move(Vector2Int.down))
+        {
+            continue;
+        }
+    }
+
+    private bool Move(Vector2Int translation)
+    {
+        Vector3Int newPosition = this.position;
+        newPosition.x += translation.x;
+        newPosition.y += translation.y;
+
+        bool valid = this.board.IsValidPosition(this, newPosition);
+
+        if (valid)
+        {
+            this.position = newPosition;
+        }
+
+        return valid;
+    }
 }
